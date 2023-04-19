3845c912d344b3165bdd52e9f54bef12d3144427
VIKASH RAM MAHURI <vikashrammahuri@users.noreply.github.com>
2023-04-19 23:17:38 +0530

    Update week8.py

--- week8.py
+++ week8.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1HvgbW7oEw3B6gK-bGFo5Cma4shtNqjsk
 """
 
-!pip install -q streamlit
-
 import streamlit as st
 
 st.title("Find the largest among the 3 numbers")
